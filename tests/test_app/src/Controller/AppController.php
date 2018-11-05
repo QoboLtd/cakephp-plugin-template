086fe40bdd7c7ff780e85e53ae4de8839a39d7dc
George Constantinou <georgec20001@gmail.com>
2018-11-05 11:38:56 +0200

    Removed Auth component loading (task #8244)

--- tests/test_app/src/Controller/AppController.php
+++ tests/test_app/src/Controller/AppController.php
@@ -8,9 +8,6 @@ class AppController extends Controller
     public function initialize()
     {
         parent::initialize();
-        $this->loadComponent('Auth', [
-            'authenticate' => ['Form']
-        ]);
 
         $this->loadComponent('Flash');
         $this->loadComponent('RequestHandler', [
